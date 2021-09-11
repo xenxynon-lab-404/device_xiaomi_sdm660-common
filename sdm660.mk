be6c25f3f4ea64522c0fb0a1252daa8ed0d796b1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-11 18:23:20 +0200

    sdm660-common: Drop product_launched_with_*.mk (1/2)
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL
    
    Change-Id: If00badd6dc12e0245c74bba12e4816361091e754
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Indicate the first api level the device has been commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 28
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
