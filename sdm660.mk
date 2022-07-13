de7f8e442ba49da0828957b4fc8c9c8b3fcb8dc5
xenxynon <flynryder427@gmail.com>
2022-07-13 08:20:11 +0530

    sdm660-common: Donot build mock service
    * 07-13 08:20:54.018 E/android.hardware.power.stats@1.0-service-mock(792): Failed to getEnergyData
    
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -348,8 +348,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service \
-    android.hardware.power-impl \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/config/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
